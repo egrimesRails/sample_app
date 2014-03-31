24c4fee8954b007314c4e219c5e93d2e3e254f4d
grimesRails <egrimes@madisoncollege.edu>
2014-03-31 10:48:28 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
+  it { should respond_to(:remember_token) }
   it { should be_valid }
   it { should respond_to(:authenticate) }
 
@@ -40,7 +40,12 @@ describe User do
       end
     end
   end
-
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+  
   describe "when email format is valid" do
     it "should be valid" do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
