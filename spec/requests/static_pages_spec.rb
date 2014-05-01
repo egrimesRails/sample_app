8a11b899e8cc0620e96b2e55591991597a366526
grimesRails <egrimes@madisoncollege.edu>
2014-04-30 22:01:39 -0500

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,7 @@ describe "Static pages" do
     it { should have_selector('h1', text: heading) }
     it { should have_title(full_title(page_title)) }
   end
+  
   describe "Home page" do
     before { visit root_path }
     
@@ -30,6 +31,17 @@ describe "Static pages" do
         user.feed.each do |item|
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
+      end
+      
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
       end
     end
   end
